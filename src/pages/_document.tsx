3e251cd347b0e1e8c9c52ad7b5de219f8641d7de
saitama1899 <eric.lorien92@gmail.com>
2024-09-13 17:12:03 +0200

    fix paths

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,18 +10,18 @@ import { ServerStyleSheet } from "styled-components";
 const App = () => (
 	<Html>
 		<link rel="icon" href="/images/icons/favicon.ico" />{" "}
-		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+		<link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-touch-icon.png" />
 		<link
 			rel="icon"
 			type="image/png"
 			sizes="192x192"
-			href="/android-chrome-192x192.png"
+			href="/images/icons/android-chrome-192x192.png"
 		/>
 		<link
 			rel="icon"
 			type="image/png"
 			sizes="512x512"
-			href="/android-chrome-512x512.png"
+			href="/images/icons/android-chrome-512x512.png"
 		/>
 		<Head />
 		<body>
