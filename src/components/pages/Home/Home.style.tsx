c03a2c3f9d9d7706a161a06825daf6dc35984556
saitama1899 <eric.lorien92@gmail.com>
2024-09-14 20:24:32 +0200

    add styles

--- src/components/pages/Home/Home.style.tsx
+++ src/components/pages/Home/Home.style.tsx
@@ -6,6 +6,8 @@ export const HomeWrapper = styled.div`
     padding: 28px 14px;
   }
   .routine {
+    height: 150px;
+    padding: 25px;
     background-color: #c7d0cd;
     background-image: 
       linear-gradient(20deg, rgba(255, 255, 255, 0.05) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.05) 50%, rgba(255, 255, 255, 0.05) 75%, transparent 75%, transparent), linear-gradient(20deg, rgba(255, 255, 255, 0.05) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.05) 50%, rgba(255, 255, 255, 0.05) 75%, transparent 75%, transparent);
